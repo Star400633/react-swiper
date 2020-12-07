5344a91103517433d99f4a9cb1104768d24840b4
liupanwang <panwang.liu@baozun.com>
2020-12-07 17:31:44 +0800

    git 测试代码

--- git.js
+++ git.js
@@ -86,6 +86,7 @@ function mergeBranch(target, crt, msg='') {
     const { stdout } = mergeRes
     const flag = stdout.indexOf('Merge conflict') > -1
     if(flag) {
+      await exec(`git merge --abort && git checkout ${crt}`)
       echo('合并发生冲突，已回退代码，请手动执行操作~')
       resolve(false)
     } else {
